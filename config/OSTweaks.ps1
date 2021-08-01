604c1c45d16158339c12010c1b9b60e551826936
keenits <86823273+keenits@users.noreply.github.com>
2021-08-01 11:07:27 -0700

    Update OSTweaks.ps1

--- config/OSTweaks.ps1
+++ config/OSTweaks.ps1
@@ -143,9 +143,9 @@ $ErrorActionPreference = 'SilentlyContinue'
         $onedrive = "$env:SYSTEMROOT\System32\OneDriveSetup.exe"
     }
     Start-Process $onedrive "/uninstall" -NoNewWindow -Wait
-    Start-Sleep -s 2
-    Stop-Process -Name "explorer"
-    Start-Sleep -s 2
+    #Start-Sleep -s 2
+    #Stop-Process -Name "explorer"
+    #Start-Sleep -s 2
     Remove-Item -Path "$env:USERPROFILE\OneDrive" -Force -Recurse
     Remove-Item -Path "$env:LOCALAPPDATA\Microsoft\OneDrive" -Force -Recurse
     Remove-Item -Path "$env:PROGRAMDATA\Microsoft OneDrive" -Force -Recurse
