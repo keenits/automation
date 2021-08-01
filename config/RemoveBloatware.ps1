3629cf400682f4d4e73f6bca5b1f29a6bef3d8d8
keenits <86823273+keenits@users.noreply.github.com>
2021-08-01 07:34:02 -0700

    Update RemoveBloatware.ps1

--- config/RemoveBloatware.ps1
+++ config/RemoveBloatware.ps1
@@ -2,6 +2,15 @@ $ErrorActionPreference = 'SilentlyContinue'
 
 Write-Output "Removing Most Provisioned Apps - Keep Basic Tools"
 Get-AppxProvisionedPackage -Online | Where-Object {$_.PackageName -notlike "*windows*"} | Where-Object {$_.PackageName -notlike "*store*"} | Remove-AppxProvisionedPackage -Online
+Get-AppxProvisionedPackage -Online | Where-Object {$_.PackageName -like "*feedbackhub*"} | Where-Object {$_.PackageName -like "*feedbackhub*"} | Remove-AppxProvisionedPackage -Online
+Get-AppxProvisionedPackage -Online | Where-Object {$_.PackageName -like "*communicationsapps*"} | Where-Object {$_.PackageName -like "*feedbackhub*"} | Remove-AppxProvisionedPackage -Online
 
-Write-output "Removing Most Apps - Keep Basic Tools"
+
+Write-Output "Removing Most Apps - Keep Basic Tools"
 Get-AppxPackage -AllUsers  | Where-Object {$_.PackageFullName -notlike "*windows*"} | Where-Object {$_.PackageFullName -notlike "*store*"} | Remove-AppxPackage
+Get-AppxPackage -AllUsers  | Where-Object {$_.PackageFullName -like "*feedbackhub*"} | Where-Object {$_.PackageFullName -like "*feedbackhub*"} | Remove-AppxPackage
+Get-AppxPackage -AllUsers  | Where-Object {$_.PackageFullName -like "*communicationsapps*"} | Where-Object {$_.PackageFullName -like "*communicationsapps*"} | Remove-AppxPackage
+
+Write-Output "Removing OneDrive"
+if (Test-Path "$env:systemroot\System32\OneDriveSetup.exe") {& "$env:systemroot\System32\OneDriveSetup.exe" /uninstall}
+if (Test-Path "$env:systemroot\SysWOW64\OneDriveSetup.exe") {& "$env:systemroot\SysWOW64\OneDriveSetup.exe" /uninstall}
