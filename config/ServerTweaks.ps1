8dafd4e0a5c5fb09019b0366bf199aa7d638960d
keenits <86823273+keenits@users.noreply.github.com>
2021-08-03 08:44:54 -0700

    Create ServerTweaks.ps1

---
+++ config/ServerTweaks.ps1
@@ -0,0 +1,138 @@
+$ErrorActionPreference = 'SilentlyContinue'
+
+
+Start-Transcript $ENV:ProgramData\OSDeploy\Logs\OSTweaks-transcript.txt
+Write-Output "**********************"
+
+
+#BIOS Time
+    Write-Output "Setting BIOS time to UTC..."
+    Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\TimeZoneInformation" -Name "RealTimeIsUniversal" -Type DWord -Value 1
+    If ((Get-WmiObject -Class Win32_ComputerSystem).PCSystemType -ne 2)
+#Desktop shortcuts
+    #Write-Output "Deleting desktop shortcuts..."
+    #Try {
+        #Remove-Item 'C:\Users\Public\Desktop\Acrobat Reader DC.lnk' -Force
+        #Remove-Item 'C:\Users\Public\Desktop\Google Chrome.lnk' -Force
+        #Remove-Item 'C:\Users\Public\Desktop\Microsoft Edge.lnk' -Force
+    #}
+    #Catch{}
+#Drive letters
+    Get-WmiObject -Class Win32_volume -Filter 'DriveType=5' | Select-Object -First 1 | Set-WmiInstance -Arguments @{DriveLetter="Z:"} | Out-Null
+#IEESC
+	  function Disable-InternetExplorerESC {
+      $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+      $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+      Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0 -Force
+      Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 1 -Force
+      Stop-Process -Name Explorer -Force
+      Write-Output "IE Enhanced Security Configuration (ESC) has been disabled." -ForegroundColor Green
+    }
+    Disable-InternetExplorerESC
+#Firewall
+    Set-NetFirewallRule -Name FPS-ICMP6-ERQ-In, WINRM-HTTP-In-TCP-PUBLIC -Enabled false
+	  Set-NetFirewallRule -Name FPS-ICMP4-ERQ-In -Profile Domain -Enabled true
+    Set-NetFirewallRule -Name RemoteTask-In-TCP,RemoteTask-RPCSS-In-TCP -Profile Domain -Enabled true
+    Set-NetFirewallRule -Name RemoteSvcAdmin-In-TCP,RemoteSvcAdmin-NP-In-TCP,RemoteSvcAdmin-RPCSS-In-TCP -Profile Domain -Enabled true
+    Set-NetFirewallRule -Name RemoteEventLogSvc-In-TCP,RemoteEventLogSvc-NP-In-TCP,RemoteEventLogSvc-RPCSS-In-TCP -Profile Domain -Enabled true
+    Set-NetFirewallRule -Name WINRM-HTTP-In-TCP,WMI-WINMGMT-In-TCP -Profile Domain -Enabled true
+#NIC settings
+    Write-Output "Disabling IPv6..."
+    Disable-NetAdapterBinding -InterfaceAlias (Get-NetAdapterBinding).Name -ComponentID ms_tcpip6
+    Write-Output "Disabling LMHOSTS Lookup..."
+    Set-ItemProperty -path "HKLM:\SYSTEM\CurrentControlSet\Services\NetBT\Parameters" EnableLMHOSTS -value 0
+    Function SetTCPIP {
+        $adapters=(gwmi win32_networkadapterconfiguration )
+            Foreach ($adapter in $adapters){
+                $adapter.settcpipnetbios(1)
+    Write-Output "Forced NetBIOS over TCP/IP..."
+    }}
+    SetTCPIP | Out-Null
+#Services
+    Set-Service "MapsBroker" -StartupType Disabled
+    Set-Service -Name WinRM -StartupType Automatic
+    Start-Service WinRM
+    schtasks /change /tn "\Microsoft\Windows\Workplace Join\Automatic-Device-Join" /disable
+    #Write-Output "Stopping and disabling Diagnostics Tracking Service..."
+    #Stop-Service "DiagTrack" -WarningAction SilentlyContinue
+    #Set-Service "DiagTrack" -StartupType Disabled
+    #Write-Output "Stopping and disabling WAP Push Service..."
+    #Stop-Service "dmwappushservice" -WarningAction SilentlyContinue
+    #Set-Service "dmwappushservice" -StartupType Disabled
+    #Write-Output "Enabling F8 boot menu options..."
+    #bcdedit /set `{current`} bootmenupolicy Legacy | Out-Null
+    #Write-Output "Disabling Remote Assistance..."
+    #Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Remote Assistance" -Name "fAllowToGetHelp" -Type DWord -Value 0
+    #Write-Output "Stopping and disabling Superfetch service..."
+    #Stop-Service "SysMain" -WarningAction SilentlyContinue
+    #Set-Service "SysMain" -StartupType Disabled
+#Start menu
+    Write-Output "Disabling recently added apps on start menu..."
+    reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Explorer" /v HideRecentlyAddedApps /t REG_DWORD /d 1 /f | Out-Null
+#System drive
+    Write-Output "Renaming system drive..."
+    Set-Volume -DriveLetter C -NewFileSystemLabel "Windows"
+#Timezone
+    Write-Output "Setting TimeZone..."
+    Set-TimeZone -Name "Pacific Standard Time"
+#UAC
+    function Disable-UserAccessControl {
+        Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Value 00000000 -Force
+        Write-Output "User Access Control (UAC) has been disabled." -ForegroundColor Green    
+    }
+    Disable-UserAccessControl
+
+############
+##Load Hive
+    Write-Output "Loading default profile reg hive..."
+    reg load HKLM\DEFAULT c:\users\default\ntuser.dat
+
+#Browser
+    Write-Output "Configuring browser settings..."
+    reg add "HKLM\DEFAULT\Software\Microsoft\Internet Explorer\Main" /v "FormSuggest Passwords" /t REG_SZ /d no /f
+    reg add "HKLM\DEFAULT\Software\Microsoft\Internet Explorer\Main" /v "FormSuggest PW Ask" /t REG_SZ /d no /f
+    reg add "HKLM\DEFAULT\Software\Microsoft\Internet Explorer\Main" /v "Start Page" /t REG_SZ /d "https://www.google.com/" /f
+    reg add "HKLM\DEFAULT\Software\Microsoft\Internet Explorer\Main" /v "Use FormSuggest" /t REG_SZ /d yes /f
+    reg add "HKLM\DEFAULT\Software\Policies\Microsoft\MicrosoftEdge\Main" /v "FormSuggest Passwords" /t REG_SZ /d - /f
+#Console
+    Write-Output "Configuring console settings..."
+    reg add "HKLM\DEFAULT\Console" /v "CursorSize" /t REG_DWORD /d 50 /f
+    reg add "HKLM\DEFAULT\Console" /v "LineWrap" /t REG_DWORD /d 1 /f
+    reg add "HKLM\DEFAULT\Console" /v "ScreenBuffer" /t REG_DWORD /d 589889686 /f
+    reg add "HKLM\DEFAULT\Console" /v "WindowSize" /t REG_DWORD  /d 4587670 /f
+    reg add "HKLM\DEFAULT\Console\%SystemRoot%_System32_WindowsPowerShell_v1.0_powershell.exe" /v CursorSize /t REG_DWORD /d 50 /f
+    reg add "HKLM\DEFAULT\Console\%SystemRoot%_System32_WindowsPowerShell_v1.0_powershell.exe" /v LineWrap /t REG_DWORD /d 1 /f
+    reg add "HKLM\DEFAULT\Console\%SystemRoot%_System32_WindowsPowerShell_v1.0_powershell.exe" /v ScreenBuffer /t REG_DWORD /d 589889686 /f
+    reg add "HKLM\DEFAULT\Console\%SystemRoot%_System32_WindowsPowerShell_v1.0_powershell.exe" /v WindowSize /t REG_DWORD  /d 4587670 /f
+#Desktop
+    Write-Output "Configuring desktop settings..."
+    reg add "HKLM\DEFAULT\Control Panel\Colors" /v Background /t REG_SZ /d "0 99 177" /f
+    reg add "HKLM\DEFAULT\Control Panel\Desktop" /v Wallpaper /t REG_SZ /f
+#Explorer
+    Write-Output "Configuring Explorer settings..."
+    reg add "HKLM\DEFAULT\Software\Microsoft\Windows\CurrentVersion\Explorer" /v ShellState /t REG_BINARY /d 240000001C28000000000000000000000000000001000000130000000000000062000000 /f
+    reg add "HKLM\DEFAULT\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v DontUsePowerShellOnWinX /t REG_DWORD /d 0 /f
+    reg add "HKLM\DEFAULT\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v Hidden /t REG_DWORD /d 1 /f
+    reg add "HKLM\DEFAULT\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v HideFileExt /t REG_DWORD /d 0 /f
+    reg add "HKLM\DEFAULT\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v LaunchTo /t REG_DWORD /d 1 /f
+    reg add "HKLM\DEFAULT\Software\Microsoft\Windows\CurrentVersion\Explorer\AutoComplete" /v "Append Completion" /t REG_SZ /d "yes" /f
+    reg add "HKLM\DEFAULT\Software\Microsoft\Windows\CurrentVersion\Explorer\CabinetState" /v FullPath /t REG_DWORD /d 1 /f
+    reg add "HKLM\DEFAULT\Software\Microsoft\Windows\CurrentVersion\Explorer\CabinetState" /v Settings /t REG_BINARY /d 0c00020b01000060000000 /f    reg add "HKLM\DEFAULT\Software\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects" /v VisualFXSetting /t REG_DWORD /d 2 /f
+#File operations
+    Write-Output "Showing file operations details..."
+    reg add "HKLM\DEFAULT\Software\Microsoft\Windows\CurrentVersion\Explorer\OperationStatusManager" /v EnthusiastMode /t REG_DWORD /d 1 /f
+#Taskbar
+    Write-Output "Setting taskbar search icon..."
+    reg add "HKLM\DEFAULT\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" /v SearchboxTaskbarMode /t REG_DWORD /d 1 /f
+    Write-Output "Cleaning up the taskbar..."
+    #reg add "HKLM\DEFAULT\Software\Microsoft\Windows\CurrentVersion\Explorer\TaskBand" /v Favorites /t REG_BINARY /d ff /f
+    #reg add "HKLM\DEFAULT\Software\Microsoft\Windows\CurrentVersion\Feeds" /v ShellFeedsTaskbarViewMode /t REG_DWORD /d 2 /f
+    #reg add "HKLM\DEFAULT\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v HideSCAMeetNow /t REG_DWORD /d 1 /f
+    #reg add "HKLM\DEFAULT\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v HideSCAVolume /t REG_DWORD /d 1 /f
+
+##Unload Hive
+    Write-Output "Unloading default profile reg hive..."
+        reg unload HKLM\DEFAULT
+    
+
+Stop-Transcript
