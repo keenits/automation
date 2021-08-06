c9bb1987fe1b45bd6013d064915d3bf22b9fd809
keenits <86823273+keenits@users.noreply.github.com>
2021-08-05 20:35:50 -0700

    Update OSDCloudMenu.ps1

--- files/OSDCloudMenu.ps1
+++ files/OSDCloudMenu.ps1
@@ -4,8 +4,8 @@ Write-Host "=================== Main Menu ====================" -ForegroundColor
 Write-Host "================ www.keenits.com =================" -ForegroundColor Yellow
 Write-Host "============== support@keenits.com ===============" -ForegroundColor Yellow
 Write-Host "==================================================" -ForegroundColor Yellow
-Write-Host "1: Zero-Touch Win10 21H1 | English | Enterprise"-ForegroundColor Yellow
-Write-Host "2: Zero-Touch Win10 20H2 | English | Enterprise" -ForegroundColor Yellow
+Write-Host "1: Zero-Touch Win10 21H1 | English | Pro"-ForegroundColor Yellow
+Write-Host "2: Zero-Touch Win10 20H2 | English | Pro" -ForegroundColor Yellow
 Write-Host "3: I'll select it myself"-ForegroundColor Yellow
 Write-Host "4: Exit`n"-ForegroundColor Yellow
 $input = Read-Host "Please make a selection"
@@ -17,8 +17,8 @@ Install-Module OSD -Force
 
 switch ($input)
 {
-    '1' { Start-OSDCloud -OSLanguage en-us -OSBuild 21H1 -OSEdition Enterprise -ZTI } 
-    '2' { Start-OSDCloud -OSLanguage en-us -OSBuild 20H2 -OSEdition Enterprise -ZTI } 
+    '1' { Start-OSDCloud -OSLanguage en-us -OSBuild 21H1 -OSEdition Pro -ZTI } 
+    '2' { Start-OSDCloud -OSLanguage en-us -OSBuild 20H2 -OSEdition Pro -ZTI } 
     '3' { Start-OSDCloud	} 
     '4' { Exit		}
 }
