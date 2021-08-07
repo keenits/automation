222debf1093174feadabe1df96984b3b658630d9
keenits <86823273+keenits@users.noreply.github.com>
2021-08-07 11:24:51 -0700

    Update ResetSvcPwds.ps1

--- config/ResetSvcPwds.ps1
+++ config/ResetSvcPwds.ps1
@@ -11,14 +11,14 @@ Write-Output "**********************"
 $usr = "@usr@"
 
 #Securing password
-if ( "@pwd@" -ne ""){
+If ( "@pwd@" -ne "") {
     $secpwd = ConvertTo-SecureString "@pwd@" -AsPlainText -Force
     }
-    else{
-        Write-Output "No password provided, exiting script"
+Else {
+    Write-Output "No password provided, exiting script"
 	Stop-Transcript
-        Exit
-        }
+    Exit
+    }
 
 Write-Verbose "Searching for $($usr) on the local system..."
 
@@ -28,30 +28,35 @@ Try {
         Write-Verbose "User $($usr) was found as a member of the local Administrators group... password reset, exiting script"
     }
 
-    Catch [Microsoft.PowerShell.Commands.PrincipalNotFoundException] {
-        If (!$ObjLocalUser) {
-            Try {
-                $ObjLocalUser = Get-LocalUser $($usr)
-                    Set-LocalUser -Name $($usr) -Password $($secpwd)
-                    Add-LocalGroupMember -Group Administrators -Member $($usr)
-                        Write-Verbose "User $($usr) was found but not a member of the local Administrators group... password reset and added to Administrators group, exiting script"
-                }
+Catch [Microsoft.PowerShell.Commands.PrincipalNotFoundException] {
+    If (!$ObjLocalUser) {
+        Try {
+            $ObjLocalUser = Get-LocalUser $($usr)
+                Set-LocalUser -Name $($usr) -Password $($secpwd)
+                Add-LocalGroupMember -Group Administrators -Member $($usr)
+                    Write-Verbose "User $($usr) was found but not a member of the local Administrators group... password reset and added to Administrators group, exiting script"
+            }
 
-                Catch [Microsoft.PowerShell.Commands.UserNotFoundException] {
-        	        If (!$ObjLocalUser) {
-                        New-LocalUser -AccountNeverExpires:$true -Password $($secpwd) -Name $($usr) -PasswordNeverExpires | Add-LocalGroupMember -Group Administrators
-                            Write-Verbose "User $($usr) was not found... created account and added to Administrators group, exiting script"
+        Catch [Microsoft.PowerShell.Commands.UserNotFoundException] {
+        	If (!$ObjLocalUser) {
+                Try {
+                    New-LocalUser -AccountNeverExpires:$true -Password $($secpwd) -Name $($usr) -PasswordNeverExpires | Add-LocalGroupMember -Group Administrators
+                        Write-Verbose "User $($usr) was not found... created account and added to Administrators group, exiting script"
+                    }
+
+                Catch {
+                    "An unspecifed error occured on user creation, exiting script" | Write-Error
                     }
                 }
-        
-                Catch {
-                    "An unspecifed error occured, exiting script" | Write-Error
-                } 
             }
+        Catch {
+            "An unspecifed error occured on user existence search, exiting script" | Write-Error
+            } 
+        }
     }
-    Catch {
-        "An unspecifed error occured, exiting script" | Write-Error
+Catch {
+    "An unspecifed error occured on group member search, exiting script" | Write-Error
     }
-    Finally {
-        Stop-Transcript
+Finally {
+    Stop-Transcript
     }
