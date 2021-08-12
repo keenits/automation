6ee6cb594e06899ce3516448f86cd3f1a0bf389d
keenits <86823273+keenits@users.noreply.github.com>
2021-08-12 12:57:45 -0700

    Update ExceptionType.ps1

--- snippets/ExceptionType.ps1
+++ snippets/ExceptionType.ps1
@@ -1,1 +1,13 @@
-$Error[0].Exception.GetType().FullName
+$error[0] | Get-Member
+
+$error[0].Exception.Message
+
+$error[0].Exception.GetType().FullName
+
+Catch {
+    Get-ErrorInformation -incomingError $_
+}
+
+Catch {
+    Write-Host $_.Exception.Message
+}
