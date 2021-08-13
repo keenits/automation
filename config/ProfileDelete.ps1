b8849c176fa6575b12de128ee5672d35f0df201e
keenits <86823273+keenits@users.noreply.github.com>
2021-08-13 08:15:51 -0700

    Update ProfileDelete.ps1

--- config/ProfileDelete.ps1
+++ config/ProfileDelete.ps1
@@ -1,19 +1,45 @@
-Start-Transcript $ENV:ProgramData\OSDeploy\Logs\DeleteProfile-transcript.txt
+Start-Transcript $ENV:ProgramData\OSDeploy\Logs\DeleteProfile-transcript.txt -Append
+Write-Output "**********************"
+
+
+Function Get-ErrorInformation {
+    [cmdletbinding()]
+    param($incomingError)
+If ($incomingError -and (($incomingError| Get-Member | Select-Object -ExpandProperty TypeName -Unique) -eq 'System.Management.Automation.ErrorRecord')) {
+    Write-Output `n"Error information:"`n
+    Write-Output `t"Exception type for catch: [$($IncomingError.Exception | Get-Member | Select-Object -ExpandProperty TypeName -Unique)]"`n 
+    
+    If ($incomingError.InvocationInfo.Line) {
+        Write-Output `t"Command: [$($incomingError.InvocationInfo.Line.Trim())]"
+    }
+
+    Else {
+        Write-Output `t"Unable to get command information! Multiple catch blocks can do this :("`n
+    }
+
+    Write-Output `t"Exception: [$($incomingError.Exception.Message)]"`n
+    Write-Output `t"Target Object: [$($incomingError.TargetObject)]"`n
+}
+
+Else {
+    Write-Output "Please include a valid error record when using this function!" -ForegroundColor Red -BackgroundColor DarkBlue
+}
+}
 
 
 Try {
     Get-WMIObject -class Win32_UserProfile | Where {$_.LocalPath -like "@Simple_Username@"} | Remove-WmiObject
     Write-Output "@Simple_Username@ profile deleted successfully, exiting script"
-    }
+}
     
-    Catch [System.IO.FileLoadException]{
-        "Profile is being used by another process, exiting script" | Write-Warning
-    }
+#Catch [System.IO.FileLoadException]{
+#    "Profile is being used by another process, exiting script" | Write-Warning
+#}
 
-    Catch {
-        "An unspecifed error occured, exiting script" | Write-Error
-    }
+Catch {
+    Get-ErrorInformation -incomingError $_
+}
 
-    Finally {
-        Stop-Transcript
-    }
+Finally {
+    Stop-Transcript
+}
