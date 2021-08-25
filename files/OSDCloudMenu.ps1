2081d3abe94908358a8b69fc74f331fee60afd3a
keenits <86823273+keenits@users.noreply.github.com>
2021-08-25 12:10:45 -0700

    Update OSDCloudMenu.ps1

--- files/OSDCloudMenu.ps1
+++ files/OSDCloudMenu.ps1
@@ -16,12 +16,12 @@ Import-Module OSD -Force
 Install-Module OSD -Force
 
 #switch ($input)
-#{
-#    '1' { Start-OSDCloud -OSLanguage en-us -OSBuild 21H1 -OSEdition Pro -SkipAutopilot -ZTI } 
-#    '2' { Start-OSDCloud -OSLanguage en-us -OSBuild 20H2 -OSEdition Pro -SkipAutopilot -ZTI } 
-#    '3' { Start-OSDCloud	} 
-#    '4' { Exit		}
-#}
-Start-OSDCloud -OSLanguage en-us -OSBuild 21H1 -OSEdition Pro -SkipAutopilot -SkipODT -ZTI
+{
+    '1' { Start-OSDCloud -OSLanguage en-us -OSBuild 21H1 -OSEdition Pro -SkipAutopilot -ZTI } 
+    '2' { Start-OSDCloud -OSLanguage en-us -OSBuild 20H2 -OSEdition Pro -SkipAutopilot -ZTI } 
+    '3' { Start-OSDCloud	} 
+    '4' { Exit		}
+}
+#Start-OSDCloud -OSLanguage en-us -OSBuild 21H1 -OSEdition Pro -SkipAutopilot -SkipODT -ZTI
 
 wpeutil reboot
