2e70b8e72969bf49f7e1dc374ff2c9ce620a7645
keenits <86823273+keenits@users.noreply.github.com>
2021-08-25 13:38:12 -0700

    Update OSDCloudMenu.ps1

--- files/OSDCloudMenu.ps1
+++ files/OSDCloudMenu.ps1
@@ -1,11 +1,11 @@
 Write-Host  -ForegroundColor Yellow "Starting Brooks' Custom OSDCloud ..."
 cls
-Write-Host "=================== Main Menu ====================" -ForegroundColor Yellow
-Write-Host "================ www.keenits.com =================" -ForegroundColor Yellow
-Write-Host "============== support@keenits.com ===============" -ForegroundColor Yellow
-Write-Host "==================================================" -ForegroundColor Yellow
-Write-Host "1: Zero-Touch Win10 21H1 | English | Pro"-ForegroundColor Yellow
-Write-Host "2: Zero-Touch Win10 20H2 | English | Pro" -ForegroundColor Yellow
+Write-Host "===================== Main Menu =======================" -ForegroundColor Yellow
+Write-Host "================ www.brookspeppin.com =================" -ForegroundColor Yellow
+Write-Host "====================@brookspeppin =====================" -ForegroundColor Yellow
+Write-Host "=======================================================" -ForegroundColor Yellow
+Write-Host "1: Zero-Touch Win10 21H1 | English | Enterprise"-ForegroundColor Yellow
+Write-Host "2: Zero-Touch Win10 20H2 | English | Enterprise" -ForegroundColor Yellow
 Write-Host "3: I'll select it myself"-ForegroundColor Yellow
 Write-Host "4: Exit`n"-ForegroundColor Yellow
 $input = Read-Host "Please make a selection"
@@ -17,11 +17,10 @@ Install-Module OSD -Force
 
 switch ($input)
 {
-    '1' { Start-OSDCloud -OSLanguage en-us -OSBuild 21H1 -OSEdition Pro -SkipAutopilot -ZTI } 
-    '2' { Start-OSDCloud -OSLanguage en-us -OSBuild 20H2 -OSEdition Pro -SkipAutopilot -ZTI } 
+    '1' { Start-OSDCloud -OSLanguage en-us -OSBuild 21H1 -OSEdition Enterprise -ZTI } 
+    '2' { Start-OSDCloud -OSLanguage en-us -OSBuild 20H2 -OSEdition Enterprise -ZTI } 
     '3' { Start-OSDCloud	} 
     '4' { Exit		}
 }
-#Start-OSDCloud -OSLanguage en-us -OSBuild 21H1 -OSEdition Pro -SkipAutopilot -SkipODT -ZTI
 
 wpeutil reboot
